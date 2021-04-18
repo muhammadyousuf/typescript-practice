578a20a208de977bc9498d3e9260c9f8b85ae8af
muhammad yousuf <muhammadyousuf327@gmail.com>
2021-04-18 08:17:11 -0700

    array and object with typescript

--- sandbox.js
+++ sandbox.js
@@ -13,3 +13,23 @@ var circle = function (daimeter) {
     return daimeter * Math.PI;
 };
 console.log(circle(7.5));
+var names = ["yousf", "rafae", "raza"];
+// names.push(3)
+// names[2] = 3
+var num = [1, 2, 3, 4];
+// num.push("yousuf")
+// num[2] = "rafae"
+var mixArray = ["yousuf", 2, 5, "raza"];
+mixArray.push("rafae");
+mixArray.push(3);
+// mixArray.push(false)
+var ninga = {
+    user: "yousf",
+    age: 24
+};
+//ninga.age = "44";
+//ninga.skills = ["programmer"]
+ninga = {
+    user: "raza",
+    age: 36
+};
--- sandbox.ts
+++ sandbox.ts
@@ -24,3 +24,31 @@ const circle = (daimeter:number) =>{
 }
 
 console.log(circle(7.5))
+
+
+let names = ["yousf", "rafae", "raza"]
+// names.push(3)
+// names[2] = 3
+
+let num = [1,2,3,4]
+// num.push("yousuf")
+// num[2] = "rafae"
+
+let mixArray = ["yousuf",2,5,"raza"];
+mixArray.push("rafae");
+mixArray.push(3);
+// mixArray.push(false)
+
+let ninga = {
+  user:"yousf",
+  age:24
+}
+
+//ninga.age = "44";
+//ninga.skills = ["programmer"]
+
+ninga = {
+  user:"raza",
+   age:36,
+  // skills:["developer"]
+}
