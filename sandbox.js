4c18dd2a4d8662c981c3fbad7f2ac68445bca099
muhammad yousuf <muhammadyousuf327@gmail.com>
2021-04-18 08:10:18 -0700

    variable and type

--- sandbox.js
+++ sandbox.js
@@ -4,3 +4,12 @@ var inputs = document.querySelectorAll('input');
 inputs.forEach(function (input) {
     console.log(input);
 });
+var username = "yousuf";
+// username = 4
+var age = 43;
+var flag = false;
+//flag = "no"
+var circle = function (daimeter) {
+    return daimeter * Math.PI;
+};
+console.log(circle(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -9,3 +9,18 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
   console.log(input);
 });
+
+let username = "yousuf";
+
+// username = 4
+
+let age = 43
+let flag = false
+
+//flag = "no"
+
+const circle = (daimeter:number) =>{
+  return daimeter  * Math.PI
+}
+
+console.log(circle(7.5))
